148cb661c31e24ab1918aa5180518c19e2f5a70c
richpingree <richpingree@fullsail.edu>
2015-08-05 18:29:32 -0500

    created MGDProject
    created MGDProject

---
+++ MGDProject/MGDProject/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  MGDProject
+//
+
+//  Copyright (c) 2015 Richard Pingree. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ MGDProject/MGDProject/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  MGDProject
+//
+
+//  Copyright (c) 2015 Richard Pingree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
